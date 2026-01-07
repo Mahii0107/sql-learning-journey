ee3284947717c7cd13aa52ad78d1bba61dda4f58
Mahi <mahipendkalkar01@gmail.com>
2026-01-07 17:20:46 +0530

    Add SQL script for null operator examples

---
+++ basics of sql/null_operator.sql
@@ -0,0 +1,11 @@
+
+
+select * from Employee
+insert into Employee
+values(9,'Jay','','Finance',73000,'2024-05-08')
+
+select * from Employee
+where Department is null
+
+select * from Employee
+where Department is not null
