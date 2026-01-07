e998ff0c8e6c1930f3de89dac9737853298167e7
Mahi <mahipendkalkar01@gmail.com>
2026-01-07 16:55:59 +0530

    Fix SQL queries in order_by.sql
    Corrected table name and fixed spelling errors in the SQL queries.

--- basics of sql/order_by.sql
+++ basics of sql/order_by.sql
@@ -1,13 +1,13 @@
 
 
-select * from [dbo].[Employees] 
+select * from [dbo].[Employee] 
 order by Salary
 
-select * from [dbo].[Employees] 
+select * from [dbo].[Employee] 
 order by Salary desc
 
-select * from [dbo].[Employees] 
+select * from [dbo].[Employee] 
 order by FirstNmae asc,Salary desc
 
-select * from [dbo].[Employees] 
+select * from [dbo].[Employee] 
 order by Department asc,Salary desc
